f44fde9443cad960c094e62ec312b91e9e02f4e0
yesung <jys_troy@naver.com>
2025-07-10 16:23:41 +0900

    .

--- src/global/outlines/Header.js
+++ src/global/outlines/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
-import { MdHome, MdDraw } from 'react-icons/md';
+import { MdHome, MdDraw, MdLogin } from 'react-icons/md';
 import color from '../styles/color';
 import fontsize from '../styles/fontsize';
 
@@ -16,6 +16,17 @@ const StyledHeader = styled.header`
   color: ${light};
   align-items: center;
   padding: 0 20px;
+  justify-content: space-between;
+
+  .left,
+  .right {
+    display: flex;
+    flex-grow: 1;
+  }
+
+  .right {
+    justify-content: right;
+  }
 
   a {
     font-size: ${big};
@@ -40,17 +51,28 @@ const StyledHeader = styled.header`
 const Header = () => {
   return (
     <StyledHeader className="layout-width">
-      <NavLink to="/">
-        <MdHome />
-        <span>메인으로</span>
-      </NavLink>
-      <NavLink
-        to="/draw"
-        className={({ isActive }) => classNames({ on: isActive })}
-      >
-        <MdDraw />
-        <span>낙서하기</span>
-      </NavLink>
+      <div className="left">
+        <NavLink to="/">
+          <MdHome />
+          <span>메인으로</span>
+        </NavLink>
+        <NavLink
+          to="/draw"
+          className={({ isActive }) => classNames({ on: isActive })}
+        >
+          <MdDraw />
+          <span>낙서하기</span>
+        </NavLink>
+      </div>
+      <div className="right">
+        <NavLink
+          to="/member/login"
+          className={({ isActive }) => classNames({ on: isActive })}
+        >
+          <MdLogin />
+          <span>로그인</span>
+        </NavLink>
+      </div>
     </StyledHeader>
   );
 };
